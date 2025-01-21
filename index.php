77b04002f1bb259d97ad4178c80eb20f9f249b76
Harish-Kumar-N <harishkumarkannan2005@gmail.com>
2025-01-21 09:28:08 +0530

    Added login page

---
+++ index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body class="d-flex justify-content-center align-items-center vh-100">
+    <div class="container" style="max-width: 475px; max-height: 500px; min-width: 400px; min-height: 350px;">
+        <div class="card p-4">
+            <div class="card-header text-center">
+            Company Name
+            </div>
+            <div class="card-body">
+                <form action="" method="post">
+                    <div>
+                        <label class="form-label mt-3">Username:</label>
+                        <input type="text" placeholder="Username" class="form-control mt-3" reqired>
+                    </div>
+                    <div>
+                        <label class="form-label mt-3">Password:</label>
+                        <input type="text" placeholder="Password" class="form-control mt-3" required>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-dark mt-5">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
