0fa33f0de5e0cc8a8edeb157777687a24ee626a2
Harish-Kumar-N <harishkumarkannan2005@gmail.com>
2025-01-21 15:49:38 +0530

    Added bill page

--- bill.php
+++ bill.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <title>Bill</title>
+    <title>Bill Information</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
