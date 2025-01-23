c116ba9ae6a6255158e6c4640defb552c61a78fe
quixel200 <blazegaming20057@gmail.com>
2025-01-23 09:34:16 +0530

    added php code

--- database_management/customer.php
+++ database_management/customer.php
@@ -1,4 +1,34 @@
-<?php include '../master/menu.php'?>
+<?php include '../master/menu.php';
+include '../master/config.php';
+
+$customer_type_details = '';
+$customer_type_query = 'select * from customer_type';
+$queryRES = mysqli_query($connection,$customer_type_query);
+while($row = mysqli_fetch_array($queryRES)){
+    $customer_type_details.='<option value="'.$row['customer_type_id'].'">'.$row['description'].'</option>';
+}
+
+if(isset($_POST['submit'])){
+    $customer = $_POST['customer'];
+    $address = $_POST['address'];
+    $mobile = $_POST['mobile'];
+    $customer_type_id = $_POST['customer_type'];
+    $stmt = $connection->prepare('insert into customer(mobile_number,name,address,customer_type_id) values (?,?,?,?)');
+    $stmt->bind_param("ssss",$mobile,$customer,$address,$customer_type_id);
+    $stmt->execute();
+}
+
+$customer_details = '';
+$customer_query = 'select c.mobile_number,c.name,c.address,ct.description from customer c join customer_type ct on ct.customer_type_id=c.customer_type_id';
+$queryEXE = mysqli_query($connection,$customer_query);
+while($row = mysqli_fetch_array($queryEXE)){
+    $customer_details.='<tr><td>'.$row['name'].'</td>';
+    $customer_details.='<td>'.$row['address'].'</td>';
+    $customer_details.='<td>'.$row['mobile_number'].'</td>';
+    $customer_details.='<td>'.$row['description'].'</td></tr>';
+}
+
+?>
 <html lang="en">
 <head>
     <title>Customer</title>
@@ -20,6 +50,7 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php echo $customer_details; ?>
                     </tbody>
                 </table>
             </div>
@@ -34,11 +65,11 @@
                         <h1 class="modal-title fs-5" id="add_customerLabel">Add Customer</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <form method="post" action="">
+                        <form method="post" action="customer.php">
                             <div class="modal-body">
                                 <div>
                                     <b><label class="form-label">Customer Name:</label></b>
-                                    <input type="text" class="form-control" name="suppiler" placeholder="Enter Customer Name" required>
+                                    <input type="text" class="form-control" name="customer" placeholder="Enter Customer Name" required>
                                 </div>
                                 <div>
                                     <b><label class="form-label mt-3">Address:</label></b>
@@ -46,16 +77,17 @@
                                 </div>
                                 <div>
                                     <b><label class="form-label mt-3">Mobile Number:</label></b>
-                                    <input type="text" class="form-control" name="contact" placeholder="Enter Contact" required>
+                                    <input type="text" class="form-control" name="mobile" placeholder="Enter Contact" required>
                                 </div>
                                 <div>
                                     <b><label class="form-label mt-3">Customer Type:</label></b>
-                                    <select class="form-select">
+                                    <select class="form-select" name="customer_type">
                                         <option selected value="">Select Customer type</option>
+                                        <?php echo $customer_type_details; ?>
                                     </select>
                                 </div>
                                 <div class="d-grid mt-3">
-                                <button type="submit" class="btn btn-success">Submit</button>
+                                <button type="submit" name="submit" class="btn btn-success">Submit</button>
                                 </div>
                             </div>
                         </form>
--- database_management/product.php
+++ database_management/product.php
@@ -1,4 +1,43 @@
-<?php include '../master/menu.php'?>
+<?php include '../master/menu.php';
+include '../master/config.php';
+
+$category_query = "select * from category";
+$product_query = "select t.name as type_name,t.color,t.size,c.name as product_name from type t join category c on c.category_id=t.category_id";
+if(isset($_POST['category_submit'])){
+    $name = $_POST['name'];
+    $stmt = $connection->prepare("insert into category(name) values (?)");
+    $stmt->bind_param("s",$name);
+    $stmt->execute();
+}
+
+if(isset($_POST['product_submit'])){
+    $name = $_POST['name'];
+    $color = $_POST['color'];
+    $size = $_POST['size'];
+    $category_id = $_POST['category_id'];
+    $stmt = $connection->prepare("insert into type(name,color,size,category_id) values (?,?,?,?)");
+    $stmt->bind_param("ssss",$name,$color,$size,$category_id);
+    $stmt->execute();
+}
+
+$category_table = '';
+$category_dropdown = '';
+$product_table = '';
+
+$queryEXE = mysqli_query($connection,$category_query);
+while($row = mysqli_fetch_array($queryEXE)){
+    $category_table.='<tr><td>'.$row['name'].'</td></tr>';
+    $category_dropdown.='<option value="'.$row['category_id'].'">'.$row['name'].'</option>';
+}
+$queryEXE1 = mysqli_query($connection,$product_query);
+while($row = mysqli_fetch_array($queryEXE1)){
+    $product_table.='<tr><td>'.$row['type_name'].'</td>';
+    $product_table.='<td>'.$row['color'].'</td>';
+    $product_table.='<td>'.$row['size'].'</td>';
+    $product_table.='<td>'.$row['product_name'].'</td></tr>';
+}
+
+?>
 <html lang="en">
 <head>
     <title>Product</title>
@@ -17,6 +56,7 @@
                         </tr>
                     </thead>
                     <tbody>
+                            <?php echo $category_table; ?>
                     </tbody>
                 </table>
             </div>
@@ -31,14 +71,14 @@
                         <h1 class="modal-title fs-5" id="add_categoryLabel">Add Category</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <form method="post" action="">
+                        <form method="post" action="product.php">
                             <div class="modal-body">
                                 <div>
                                     <b><label class="form-label">Category Name:</label></b>
                                     <input type="text" class="form-control" name="name" placeholder="Enter Category Name" required>
                                 </div>
                                 <div class="d-grid mt-3">
-                                <button type="submit" class="btn btn-success">Submit</button>
+                                <button type="submit" class="btn btn-success" name="category_submit">Submit</button>
                                 </div>
                             </div>
                         </form>
@@ -62,6 +102,7 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php echo $product_table; ?>
                     </tbody>
                 </table>
             </div>
@@ -76,7 +117,7 @@
                         <h1 class="modal-title fs-5" id="add_productLabel">Add Product</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <form method="post" action="">
+                        <form method="post" action="product.php">
                             <div class="modal-body">
                                 <div>
                                     <b><label class="form-label">Product Name:</label></b>
@@ -92,12 +133,13 @@
                                 </div>
                                 <div>
                                     <b><label class="form-label mt-3">Category:</label></b>
-                                    <select class="form-select" name="category">
+                                    <select class="form-select" name="category_id">
                                         <option selected value="">Select Category</option>
+                                        <?php echo $category_dropdown; ?>
                                     </select>
                                 </div>
                                 <div class="d-grid mt-3">
-                                <button type="button" class="btn btn-success">Submit</button>
+                                <button type="submit" class="btn btn-success" name="product_submit">Submit</button>
                                 </div>
                             </div>
                         </form>
